3545661f0a008e4a4f890f120c72a76cc5e2e291
Igor Bueno <igor_mcb@hotmail.com>
2018-09-02 16:18:02 -0400

    Mudança de porta

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-const PORT = 3000;
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor express iniciado na porta ${PORT}`);
