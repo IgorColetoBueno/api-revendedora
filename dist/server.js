4b31ad95b9237084ebfb6af55c4c2217e02802ef
Igor Bueno <igor_mcb@hotmail.com>
2018-09-02 17:44:10 -0400

    Mais uma

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = 3000;
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 app_1.app.listen(PORT, () => {
     console.log(`Servidor express iniciado na porta ${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 
 const PORT = process.env.PORT ? process.env.PORT : 3000;
-
+ 
 app.listen(PORT, () => {
   console.log(`Servidor express iniciado na porta ${PORT}`);
 });
